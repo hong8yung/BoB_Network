2c0f32170668f280b23d2d219666f71a75d01f7e
hong8yung <hoyung342@gmail.com>
2017-01-11 00:02:06 -0800

    sniff_loop example

--- testlibtins/main.cpp
+++ testlibtins/main.cpp
@@ -1,27 +1,29 @@
-#include <iostream>
 #include <tins/tins.h>
-#include <cassert>
-#include <string>
 
 using namespace Tins;
 
-bool callback(const PDU &pdu) {
-    const IP &ip = pdu.rfind_pdu<IP>(); // Find the IP layer
-    const TCP &tcp = pdu.rfind_pdu<TCP>(); // Find the TCP layer
-    std::cout << ip.src_addr() << ':' << tcp.sport() << " -> "
-              << ip.dst_addr() << ':' << tcp.dport() << std::endl;
-    return true;
+bool doo(PDU&){
+    return false;
 }
 
-int main() {
-    //Sniffer("net0").sniff_loop(callback);
-    SnifferConfiguration config;
-    config.set_filter("port 80");
-    config.set_promisc_mode(true);
-    config.set_snap_len(400);
-    Sniffer sniffer("net0", config);
-    sniffer.set_filter("ip src 192.168.0.1");   // set filter
-    PDU *some_pdu = sniffer.next_packet();
+struct foo{
+    void bar(){
+        SnifferConfiguration config;
+        config.set_promisc_mode(true);
+        config.set_filter(("ip src 192.168.0.100"));
+        Sniffer sniffer("ens33", config);
 
-    delete some_pdu;
+        sniffer.sniff_loop(make_sniffer_handler(this, &foo::handle));
+
+        sniffer.sniff_loop(doo);
+    }
+
+    bool handle(PDU&){
+        return false;
+    }
+};
+
+int main() {
+    foo f;
+    f.bar();
  }
