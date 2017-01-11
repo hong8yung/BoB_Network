722713621730b6b41b7392cfb5e0bb902704ae5c
hong8yung <hoyung342@gmail.com>
2017-01-11 01:20:39 -0800

    arp sending and recv

--- testlibtins/main.cpp
+++ testlibtins/main.cpp
@@ -14,7 +14,18 @@ bool count_packets(const PDU &){
 }
 
 main(){
-    FileSniffer sniffer("/tmp/some_pcap_file.pcap");
-    sniffer.sniff_loop(count_packets);
-    cout << "There are " << counter << "packets in the pcap file" << endl;
+    //to resolve
+    IPv4Address to_resolve("192.168.231.2"); // gateway ip addr
+    NetworkInterface iface(to_resolve);
+    auto info = iface.addresses();
+    EthernetII eth = ARP::make_arp_request(to_resolve, info.ip_addr, info.hw_addr);
+
+    //the sender
+    PacketSender sender;
+    unique_ptr<PDU> response(sender.send_recv(eth, iface));
+
+    if(response){
+        const ARP &arp = response->rfind_pdu<ARP>();
+        cout << "Hardware address: " << arp.sender_hw_addr() << endl;
+    }
 }
