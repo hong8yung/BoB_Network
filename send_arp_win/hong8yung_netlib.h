db0c6e46a0082d2d6773498177a51941da81c393
hong8yung <hoyun342@gmail.com>
2017-01-15 17:36:38 +0900

    save for dinner

--- send_arp_win/hong8yung_netlib.h
+++ send_arp_win/hong8yung_netlib.h
@@ -139,3 +139,38 @@ typedef struct libnet_tcp_hdr
 	u_int16_t th_sum;         /* checksum */
 	u_int16_t th_urp;         /* urgent pointer */
 }TCP_HDR;
+
+/*
+*  ARP header
+*  Address Resolution Protocol
+*  Base header size: 8 bytes
+*/
+typedef struct libnet_arp_hdr
+{
+	u_int16_t ar_hrd;         /* format of hardware address */
+#define ARPHRD_NETROM   0   /* from KA9Q: NET/ROM pseudo */
+#define ARPHRD_ETHER    1   /* Ethernet 10Mbps */
+#define ARPHRD_EETHER   2   /* Experimental Ethernet */
+#define ARPHRD_AX25     3   /* AX.25 Level 2 */
+#define ARPHRD_PRONET   4   /* PROnet token ring */
+#define ARPHRD_CHAOS    5   /* Chaosnet */
+#define ARPHRD_IEEE802  6   /* IEEE 802.2 Ethernet/TR/TB */
+#define ARPHRD_ARCNET   7   /* ARCnet */
+#define ARPHRD_APPLETLK 8   /* APPLEtalk */
+#define ARPHRD_LANSTAR  9   /* Lanstar */
+#define ARPHRD_DLCI     15  /* Frame Relay DLCI */
+#define ARPHRD_ATM      19  /* ATM */
+#define ARPHRD_METRICOM 23  /* Metricom STRIP (new IANA id) */
+#define ARPHRD_IPSEC    31  /* IPsec tunnel */
+	u_int16_t ar_pro;         /* format of protocol address */
+	u_int8_t  ar_hln;         /* length of hardware address */
+	u_int8_t  ar_pln;         /* length of protocol addres */
+	u_int16_t ar_op;          /* operation type */
+#define ARPOP_REQUEST    1  /* req to resolve address */
+#define ARPOP_REPLY      2  /* resp to previous request */
+#define ARPOP_REVREQUEST 3  /* req protocol address given hardware */
+#define ARPOP_REVREPLY   4  /* resp giving protocol address */
+#define ARPOP_INVREQUEST 8  /* req to identify peer */
+#define ARPOP_INVREPLY   9  /* resp identifying peer */
+							  /* address information allocated dynamically */
+}ARP_HDR;
--- send_arp_win/main.cpp
+++ send_arp_win/main.cpp
@@ -5,24 +5,46 @@
 #include <stdio.h>
 #include <windows.h>
 #include <IPHlpApi.h>
+#include <string.h>
 
 #pragma comment(lib, "iphlpapi.lib")
 
-void print_IPinfo();
-void print_AdapterInfo();
+void print_MAC(u_int8_t * mac);
+void print_IP(unsigned long ip);
+void getAttackerInfo(ULONG* attacker_ip, ULONG* gateway_ip, u_int8_t* attacker_mac, char* spNetDevName);
+u_int8_t* getVictim_MAC(pcap_t* pDes, ULONG victim_ip);
+pcap_t* connectIface(char * spNetDevName);
 
-int main()
+int main(int argc, char *argv[])
 {
+	pcap_t* pDes = NULL;
+	ULONG victim_ip, attacker_ip, gateway_ip;
+	u_int8_t  victim_mac[ETHER_ADDR_LEN], attacker_mac[ETHER_ADDR_LEN];
+	char * spNetDevName = NULL;
+	if (inet_pton(AF_INET, argv[1], &victim_ip) != 1) {	// victim ip addr String to ULONG
+		printf("[-] Invalid ip address\n");
+		exit(1);
+	}
+	pDes = connectIface(spNetDevName);	// Connect PCAP interface
 	
-	print_AdapterInfo();
+	getAttackerInfo(&attacker_ip, &gateway_ip, attacker_mac, spNetDevName);
+
+	memcpy(victim_mac, getVictim_MAC(pDes, victim_ip), sizeof(victim_mac));	// get victim mac
+	
+	printf("sizeof victim_mac : %d\n", sizeof(victim_mac));
+
+	//print_AdapterInfo();
 	//print_IPinfo();
+
+	if(pDes) pcap_close(pDes);
 	
 	return 0;
 }
-void print_AdapterInfo() {	
+
+void getAttackerInfo(ULONG* attacker_ip, ULONG* gateway_ip, u_int8_t* attacker_mac, char* spNetDevName) {
 	PIP_ADAPTER_INFO pAdapterInfo;
 	pAdapterInfo = (IP_ADAPTER_INFO *)malloc(sizeof(IP_ADAPTER_INFO));
-	
+
 	ULONG buflen = sizeof(IP_ADAPTER_INFO);
 	DWORD dwRetVal;
 
@@ -41,12 +63,20 @@ void print_AdapterInfo() {
 
 	PIP_ADAPTER_INFO pAdapter = pAdapterInfo;
 	while (pAdapter) {
+		if (strstr(spNetDevName, pAdapter->AdapterName)) {
+			inet_pton(AF_INET, pAdapter->IpAddressList.IpAddress.String, attacker_ip);
+			inet_pton(AF_INET, pAdapter->GatewayList.IpAddress.String, gateway_ip);
+			memcpy(attacker_mac, pAdapter->Address, 6);
+		}
+		
 		printf("\tAdapter Name : \t%s\n", pAdapter->AdapterName);
 		printf("\tAdapter Desc : \t%s\n", pAdapter->Description);
 		printf("\tAdapter Addr : \t%s\n", pAdapter->Address);
-		printf("\IP Address : \t%s\n", pAdapter->IpAddressList.IpAddress.String);
+		printf("\tIP Address : \t%s\n", pAdapter->IpAddressList.IpAddress.String);
 		printf("\tIP Mask : \t%s\n", pAdapter->IpAddressList.IpMask.String);
+		print_MAC((uint8_t *)(pAdapter->Address));
 		printf("\tGateway : \t%s\n", pAdapter->GatewayList.IpAddress.String);
+		
 
 		pAdapter = pAdapter->Next;
 	}
@@ -136,6 +166,76 @@ void print_IPinfo() {
 		pFixedInfo = NULL;
 	}
 }
+
+pcap_t* connectIface(char * spNetDevName) {
+	pcap_if_t *alldevs, *d;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	pcap_t *pDes;
+	bpf_u_int32 mask, net;
+
+	int res, i, inum;
+	struct pcap_pkthdr *header;
+	const u_char * pkt_data;
+	ETHER_HDR *ether_header;
+	IPv4_HDR *ip_header;
+	TCP_HDR *tcp_header;
+
+	void print_IP(unsigned long ip);
+	void print_MAC(UCHAR * mac);
+
+	// Retrieve the device list from the local machine
+	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL, &alldevs, errbuf) == -1) {
+		fprintf(stderr, "Error in pcap_findalldev_ex: %s\n", errbuf);
+		exit(1);
+	}
+
+	/* Print the list */
+	for (d = alldevs, i = 0; d != NULL; d = d->next)
+	{
+		printf("%d device : %s", ++i, d->name);
+		
+		if (d->description)
+			printf(" (%s)\n", d->description);
+		else
+			printf(" (No description available)\n");
+	}
+
+	printf("enter the interface number: ");
+	scanf_s("%d", &inum);
+	for (d = alldevs, i = 0; i<inum - 1; d = d->next, i++); // jump to the i-th dev
+
+	spNetDevName = d->name;
+
+	if (pcap_lookupnet(spNetDevName, &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", spNetDevName, errbuf);
+		net = 0;
+		mask = 0;
+	}
+
+	pDes = pcap_open_live(spNetDevName, 1500, 1, 1, errbuf);
+	if (0 == pDes) {
+		printf("[-]Error : [%s]\n", errbuf);
+	}
+	else {
+		printf("[+]Network Device Ready!\n");
+	}
+
+	
+	return pDes;
+}
+
+
+u_int8_t* getVictim_MAC(pcap_t* pDes, ULONG victim_ip) {
+	ETHER_HDR ehp;
+	ARP_HDR ahp;
+
+	memset(ehp.ether_dhost, 0xff, ETHER_ADDR_LEN);	// set broadcast
+	//memset()
+	return 0;
+	//pcap_sendpacket(pDes,,)
+}
+
 
 void print_MAC(u_int8_t * mac) {
 	for (int i = 0; i<6; i++) {
