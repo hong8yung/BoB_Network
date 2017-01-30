d5947051604aedc1a29817b72562496f9cb96296
hong8yung <hoyung342@gmail.com>
2017-01-30 05:01:50 -0800

    save

--- wireless_test/main.cpp
+++ wireless_test/main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 #include <tins/tins.h>
+#include <unordered_map>
 
 using namespace std;
 using namespace Tins;
 
+unordered_map<string, string> ap;
+
+void print_window(unordered_map<string, string> ap){
+    for(auto i = ap.begin(); i!=ap.end(); i++){
+        cout << "bssid : " << i->first << endl;
+    }
+
+}
+
+void print_beacon(const Dot11Beacon &beacon){
+    cout << "bssid : " << beacon.addr2() << endl;
+    cout << "essid : " << beacon.ssid() << endl;
+}
+
+bool callback(const PDU &pdu) {
+
+    const RadioTap &tap = pdu.rfind_pdu<RadioTap>();
+    const Dot11Beacon &beacon = pdu.rfind_pdu<Dot11Beacon>();
+
+    if(ap.find(beacon.addr2()) == ap.end()){
+        ap.insert(pair<string, string>(beacon.addr2(),beacon.ssid()));
+    }else{
+
+    }
+
+    print_beacon(beacon);
+
+    return true;
+}
+
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    // need use number
+    string driv_name;
+
+    if(!argv[1]) driv_name = "net0"; //default drive name 'net0'
+    else driv_name = argv[1];
+
+    cout << driv_name << endl;
+
+    Sniffer(driv_name).sniff_loop(callback);
+
     return 0;
 }
